1fd769a6efc35f03923498b44640c4e88d93b804
DiViNeSiC <divine.beatbox@gmail.com>
2020-10-18 20:25:09 +0330

    Updating Schedules Models

--- Models/Schedules/movement.js
+++ Models/Schedules/movement.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const path = require('path')
+
+const gifBasePath = 'Uploads/MovementGifs'
 
 const movementSchema = new mongoose.Schema({ 
     name: {
@@ -15,4 +18,13 @@ const movementSchema = new mongoose.Schema({
     hints: String
 })
 
+movementSchema.pre('save', function() {
+    if (this.gifName !== '' && this.gifName != null) {
+        return (
+            this.gifPath = path.join('/', gifBasePath, this.gifName)
+        )
+    }
+})
+
 module.exports = mongoose.model('Movement', movementSchema)
+module.exports.gifBasePath = gifBasePath
